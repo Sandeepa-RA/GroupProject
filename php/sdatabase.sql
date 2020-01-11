0b836dfe0b4fabaaed390ed76b6401f4b193ec8b
Sandeepa-RA <sandeeparanatungavcg@gmail.com>
2020-01-12 01:13:30 +0530

    Updated

---
+++ php/sdatabase.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS `user` (
+  `sid` int(10) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `quantity` int(10) NOT NULL,
+  `price` int(10) NOT NULL,
+  PRIMARY KEY (`sid`)
+)
---
+++ php/stockadd.php
@@ -0,0 +1,57 @@
+<?php require_once('connect.php'); ?>
+<html>
+    <head>
+        <title>Stock</title>
+        <link rel="stylesheet" href="../css/commonstyle.css" type="text/css">
+        <link rel="stylesheet" href="../css/stockaddstyle.css" type="text/css">
+    </head>
+    <body>
+        <?php include '../html/header.html'; ?>
+        <?php include '../html/navigation.html'; ?>
+        <div class=content>
+            <div class=formArea>    
+                    <h2>Add New Stock</h2>
+                
+                <form action='' method ='POST'>
+                <div class="text_box">
+                    Stock ID:
+                    <input type="text" placeholder="ID" name="sid" required>
+                </div>
+                <div class=text_box>
+                    Stock Name:
+                    <input type="text" placeholder="Name" name="name" reqiired>
+                </div>
+                <div class="text_box">
+                    Quantity:
+                    <input type="text" placeholder="Quantity" name="quantity" required>
+                </div>
+                <div class="text_box">
+                    Unit Price:
+                    <input type="text" placeholder="Price" name="price" required>
+                </div>  
+                    <input type="submit" value="Save" name="ssubmit" class=button>
+                    <a href="stockview.php"><button type="button" class=button>View</button></a>
+                
+                </div>
+                
+                </form>
+            
+            </div>
+       
+    </body>
+</html>
+
+<?php
+	if(isset($_POST['ssubmit'])){
+
+	$sql = "INSERT INTO user (sid,name,quantity,price) VALUES ('".$_POST['sid']."','".$_POST['name']."','".$_POST['quantity']."','".$_POST['price']."')";
+
+	$result = mysqli_query($connection,$sql);
+	if($result)
+echo"<script> alert('Stock Added Sucessfully') </script>";
+else
+echo"failed";
+
+}
+
+?>
