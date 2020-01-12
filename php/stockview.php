670a7cab44c587dc02c371a62e04dee7edb9135f
Sandeepa-RA <sandeeparanatungavcg@gmail.com>
2020-01-12 14:23:41 +0530

    Updated till Stock View

---
+++ php/stockview.php
@@ -0,0 +1,58 @@
+<?php require_once('connect.php'); ?>
+<html>
+        <?php
+        $sql = "SELECT * FROM user";
+        mysqli_query($connection, $sql);
+        $result = mysqli_query($connection,$sql);
+
+        if($result){
+        //echo "Sucessfull";
+        }
+        else{
+        echo"failed";	
+        }
+
+
+
+
+        ?>
+    <head>
+        <title>Stock View</title>
+        <link rel="stylesheet" href="../css/commonstyle.css" type="text/css">
+        <link rel="stylesheet" href="../css/stockviewstyle.css" type="text/css">
+    </head>
+    <body>
+        <?php include '../html/header.html'; ?>
+        <?php include '../html/navigation.html'; ?>
+
+            <div class=content>
+                    <h2>Current Stock</h2>
+                    <table>
+                        <tr>
+                            <td>Stock ID</td>
+                            <td>Name</td>
+                            <td>Quantity</td>
+                            <td>Unit Price</td>
+                            <td>Stock Update</td>
+                            <td>Stock Delete</td>
+                        </tr>
+                        <?php
+                        while($row=mysqli_fetch_assoc($result)){
+                        ?>
+                              <tr>
+                                <td><?php echo $row['sid'] ?></td>
+                                <td><?php echo $row['name'] ?></td>
+                                <td><?php echo $row['quantity'] ?></td>
+                                <td><?php echo $row['price'] ?></td>
+                                <?php echo "<td><a href =update.php?id='".$row['sid']."' > update </a> </td>"?>
+                                <?php echo "<td><a href =delete.php?id='".$row['sid']."' > delete </a> </td>"?>
+                            </tr>
+                        <?php
+                        }
+                        ?>
+                    </table>
+                    <br><a href="stockadd.php"><button class=button type="button">Stock Add</button></a>
+                
+            </div>
+    </body>
+</html>
